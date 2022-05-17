1edea094cfa6faf9703f4306756a585c882ceb72
I'm_Zee <62513424+I-m-Zee@users.noreply.github.com>
2022-05-17 11:08:43 +0500

    DNA
    Sol

---
+++ DNApairing.js
@@ -0,0 +1,19 @@
+function pairElement(str) {
+  let newArr = str.split("")
+  //return newArr;
+  let emptyArr = [];
+  newArr.forEach(function(x){
+     if(x=='G'){
+         emptyArr.push([x,'C']);
+     }else if(x=='C'){
+         emptyArr.push([x,'G'])
+     }else if(x=='A'){
+         emptyArr.push([x,'T'])
+     }else if(x=='T'){
+         emptyArr.push([x,'A'])
+     }
+  });
+  return emptyArr
+}
+
+pairElement("GCG");
