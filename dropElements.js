649e8a4fe36b490d26e111120f43e10d3a6aed76
I'm_Zee <62513424+I-m-Zee@users.noreply.github.com>
2022-05-18 14:54:08 +0500

    Update dropElements.js

--- dropElements.js
+++ dropElements.js
@@ -1,4 +1,5 @@
-let bool = false;
+function dropElements(arr, func) {
+    let bool = false;
     for(let i=0; i<arr.length; i++){
         bool = func(arr[i]);
         if(bool){
@@ -10,3 +11,13 @@ let bool = false;
     return arr;}else{
         return []
     }
+    
+    // return arr
+    // .filter((x, index)=>{
+    //     if(func(x)){
+    //         return arr.splice(0,index)
+    //     }
+    // })
+}
+
+console.log(dropElements([0, 1, 0, 1], function(n) {return n === 1;}));
