62fef013a22c4bdee412d4b3174c6282e124b82a
I'm_Zee <62513424+I-m-Zee@users.noreply.github.com>
2022-05-19 12:34:00 +0500

    ROT13
    Solution

---
+++ ROT13.js
@@ -0,0 +1,8 @@
+function rot13(str) {
+     const originalAlpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+     const cipher = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"
+    return str.replace(/[a-z]/gi, letter => cipher[originalAlpha.indexOf(letter)])
+
+}
+
+rot13("SERR PBQR PNZC");
